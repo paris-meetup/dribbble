6434cea9361fe3da029fa47a66be2f385cb66488
Lucas Bonomi <lucas.bonomi@gmail.com>
2015-05-11 01:01:12 +0200

    Design done :sunglasses:, firebase :fire:

--- 2015/scripts/main.js
+++ 2015/scripts/main.js
@@ -47,8 +47,8 @@ function fetchUserInformations(_name){
 };
 
 function isValidEmailAddress(emailAddress) {
-    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    return pattern.test(emailAddress);
+  var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+  return pattern.test(emailAddress);
 };
 
 function emailDetection(){
@@ -63,14 +63,21 @@ function emailDetection(){
 
 function datePicker(){
   $('input[type="checkbox"]').on('click', function(){
-      $('input[type="button"]').prop('disabled', false);
+    $('input[type="button"]').prop('disabled', false);
   })
 }
 
 function confirmFirstStep(){
-  $('input[type="button"]').on('click', function(){
+  $('input[type="button"]').on('click', function(e){
+    e.preventDefault();
+    
     $("#inscription").addClass('hidden');
-    $("#thankyou").removeClass('hidden');
+    $("#load").removeClass('hidden');
+
+    setTimeout(function(){
+      $("#thankyou").removeClass('hidden');
+      $("#load").addClass('hidden');
+    }, 1258)
 
     setupThankyou();
   })
@@ -84,6 +91,28 @@ function confirmFirstStep(){
   }
 }
 
+function showMore(){
+  $('#showMore').on('click', function(e){
+    e.preventDefault();
+    if($(this).text()=='Voir tous ...'){
+      $(this).empty().append('Voir moins..');
+      $('.user-list').animate({
+        height: "1200px",
+      }, 0, function() {}).css('overflow','auto');
+    }
+    else{
+      $(this).empty().append('Voir tous ...');
+      $('.user-list').removeClass('open');
+      $('.user-list').animate({
+        height: "210px",
+        scrollTop: 0
+      }, 0, function() {}).css('overflow','hidden');
+      $('.user-list ul').animate({ scrollTop: 0}, 800, 'swing');
+    }
+
+  })
+}
+
 function form(){
   addArobaze();
   getUserName();
@@ -95,6 +124,7 @@ function form(){
 function deploy(){
   init();
   form();
+  showMore();
 };
 
 
