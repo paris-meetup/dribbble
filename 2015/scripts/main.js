9616229bda18e18d3d366bbb1f1d5c8f323e6078
Lucas Bonomi <lucas.bonomi@gmail.com>
2015-05-11 01:49:19 +0200

    Cookie added, in order to prevent multi votes

--- 2015/scripts/main.js
+++ 2015/scripts/main.js
@@ -1,4 +1,9 @@
+!function(e){"function"==typeof define&&define.amd?define(["jquery"],e):"object"==typeof exports?module.exports=e(require("jquery")):e(jQuery)}(function(e){function n(e){return u.raw?e:encodeURIComponent(e)}function o(e){return u.raw?e:decodeURIComponent(e)}function i(e){return n(u.json?JSON.stringify(e):String(e))}function t(e){0===e.indexOf('"')&&(e=e.slice(1,-1).replace(/\\"/g,'"').replace(/\\\\/g,"\\"));try{return e=decodeURIComponent(e.replace(c," ")),u.json?JSON.parse(e):e}catch(n){}}function r(n,o){var i=u.raw?n:t(n);return e.isFunction(o)?o(i):i}var c=/\+/g,u=e.cookie=function(t,c,s){if(arguments.length>1&&!e.isFunction(c)){if(s=e.extend({},u.defaults,s),"number"==typeof s.expires){var a=s.expires,d=s.expires=new Date;d.setMilliseconds(d.getMilliseconds()+864e5*a)}return document.cookie=[n(t),"=",i(c),s.expires?"; expires="+s.expires.toUTCString():"",s.path?"; path="+s.path:"",s.domain?"; domain="+s.domain:"",s.secure?"; secure":""].join("")}for(var f=t?void 0:{},p=document.cookie?document.cookie.split("; "):[],l=0,m=p.length;m>l;l++){var x=p[l].split("="),g=o(x.shift()),j=x.join("=");if(t===g){f=r(j,c);break}t||void 0===(j=r(j))||(f[g]=j)}return f};u.defaults={},e.removeCookie=function(n,o){return e.cookie(n,"",e.extend({},o,{expires:-1})),!e.cookie(n)}});
+
+
+var myDataRef = new Firebase('https://dbbb-parismeetup.firebaseio.com/');
 var _isDisabled = false;
+
 
 $.fn.selectRange = function(start, end) {
   if(!end) end = start; 
@@ -17,7 +22,17 @@ $.fn.selectRange = function(start, end) {
 };
 
 function init(){
+  var dbbbColor = $('input[type="button"]').css('background-color');
   $('input[type="button"],input[type="email"],input[type="checkbox"]').prop('disabled', _isDisabled);
+
+  setTimeout(function(){
+    if ($.cookie('alreadyIn')=='1') {
+      $('fieldset').css('border-color', dbbbColor);
+      $('.separator-x').css('background-color', dbbbColor);
+      $('input[type="button"]').parent().prepend('<h2 style="color:'+dbbbColor+'">You are already in the list</h2>');
+      $('input[type="button"').remove();
+    }
+  }, 100);
 }
 
 
@@ -64,13 +79,25 @@ function emailDetection(){
 function datePicker(){
   $('input[type="checkbox"]').on('click', function(){
     $('input[type="button"]').prop('disabled', false);
+
+    if($('#jeudi-25-juin').is(':checked')){
+      $('#dates').attr('value','jeudi-25-juin')
+    }
+    if($('#vendredi-26-juin').is(':checked')){
+      $('#dates').attr('value','vendredi-26-juin')
+    }
+    if($('#jeudi-25-juin').is(':checked') && $('#vendredi-26-juin').is(':checked')){
+      $('#dates').attr('value','jeudi / vendredi')
+    }
+
   })
 }
 
 function confirmFirstStep(){
   $('input[type="button"]').on('click', function(e){
     e.preventDefault();
-    
+
+
     $("#inscription").addClass('hidden');
     $("#load").removeClass('hidden');
 
@@ -84,10 +111,20 @@ function confirmFirstStep(){
 
   function setupThankyou(){
     var userName = $('input[type="text"]').val();
+    var userEmail = $('input[type="email"]').val();
     var userImage = $('#detector').val();
+    var selectedDates = $('#dates').val();
+
+
 
     $('#userName').empty().append(userName);
     $('#userImage').attr('src', userImage);
+
+    // Push user data to firebase 
+    myDataRef.push({name: userName, avatar: userImage, email: userEmail, dates: selectedDates });
+
+    // Set a cookie to avoid the user to readd his email
+    $.cookie('alreadyIn', '1', { expires: 365 });
   }
 }
 
