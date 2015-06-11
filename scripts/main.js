3a90a6d43acc815dc3190ff5f008d6694e79c7c4
Lucas Bonomi <lucas.bonomi@tigerlilyapps.com>
2015-06-11 15:13:19 +0200

    update script counter

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,1 @@
-$(function() {
-  var e = new Firebase("https://dbbb-parismeetup.firebaseio.com/atendees");
-  e.on("child_added", function(e) {
-    var a = e.val();
-    $("ul#list").append('<li><a href="http://dribbble.com/' + a.name.replace("@", "") + '" target="_blank"><img src="' + a.avatar + '" /></a></li>');
-
-    setTimeout(function() {
-      $("#howmuch").empty().fadeIn().append($("#list li").length);
-    }, 2000)
-  }, function(e) {
-    console.log("The read failed: " + e.code)
-  });
-
-  
-});
+$(function() { var e = new Firebase("https://dbbb-parismeetup.firebaseio.com/atendees"); e.on("child_added", function(e) { var a = e.val(); $("ul#list").append('<li><a href="http://dribbble.com/' + a.name.replace("@", "") + '" target="_blank"><img src="' + a.avatar + '" /></a></li>'); setTimeout(function() { $("#howmuch").empty().fadeIn().append($("#list li").length); }, 2000) }, function(e) { console.log("The read failed: " + e.code) }); });
